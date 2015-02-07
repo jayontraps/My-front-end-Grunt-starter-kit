4b6795913521d3ca331abbbf3dc820e4949e6609
Jason Righelato <jayontraps@gmail.com>
2015-02-07 15:15:22 +0000

    adding rem based mobile first and getActiveMQ

--- js/main.js
+++ js/main.js
@@ -1,1 +1,99 @@
-console.log("hello from main.js");
+(function($,window){
+  
+  window.getActiveMQ = function()
+	{
+		$('<div id="getActiveMQ-watcher"></div>')
+			.appendTo('body')
+			.hide();
+	
+		var computed = window.getComputedStyle,
+			watcher = document.getElementById('getActiveMQ-watcher');
+			if ( 'currentStyle' in watcher )
+			{
+				window.getActiveMQ = function()
+				{
+					// return watcher.currentStyle['fontFamily'].replace(/['"]/g,'');
+					return watcher.currentStyle.fontFamily.replace(/['"]/g,'');
+				};
+			}
+			else if ( computed )
+			{
+				window.getActiveMQ = function()
+				{
+					return computed( watcher, null ).getPropertyValue( 'font-family' ).replace(/['"]/g,'');
+				};
+			}
+			else
+			{
+				window.getActiveMQ = function()
+				{
+					return 'unknown';
+				};
+			}
+			return window.getActiveMQ();
+	};
+
+}(jQuery, window));
+
+
+// raw resize
+// $(window).on('resize', function() {
+// 	console.log('breakpoint is ' + getActiveMQ());
+// });
+
+
+// limit resize firing
+// var resizeId;
+// $(window).on('resize', function(e) {
+//     clearTimeout(resizeId);
+//     resizeId = setTimeout(doneResizing, 250);
+    
+// });
+
+// function doneResizing(){
+//   console.log('breakpoint is ' + getActiveMQ());  
+// }
+
+
+// 3rd time lucky...
+// debulked onresize handler - https://github.com/louisremi/jquery-smartresize
+function on_resize(c,t){
+	onresize=function(){
+		clearTimeout(t);
+		t=setTimeout(c,100);
+	};
+	return c;
+}
+
+on_resize(function() {
+  console.log('breakpoint is ' + getActiveMQ()); 
+})(); // self executing on page load 
+
+
+// just on resize
+// on_resize(function() {
+//   // handle the resize event here
+//   console.log('breakpoint is ' + getActiveMQ());  
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/plugins.js
+++
@@ -1,5 +0,0 @@
-console.log("hello from plugins.js");
-
-var blood = function(e) {
-	e.preventDefault();
-};
